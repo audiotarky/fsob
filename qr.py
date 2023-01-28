1fbb736438e1245f2c571135c30ef63ba130d5ac
Simon Metson <simon@metsonet.co.uk>
2023-01-28 23:54:00 +0000

    DRY print out code

--- qr.py
+++ qr.py
@@ -10,11 +10,17 @@ def save_qr(qr, source, colour="", with_image=True):
         "back_color": "white",
     }
     if with_image:
-        kwargs["embeded_image_path"] = f"site/fsob_logo{colour}.png"
+        kwargs["embeded_image_path"] = f"docs/fsob_logo{colour}.png"
     img = qr.make_image(image_factory=StyledPilImage, **kwargs)
     name = f"fsob_qr_{source}{colour}.png"
     img.save(f"docs/{name}")
     return name
+
+
+def print_image(url, image):
+    print(f'<a href="{url}">', end="")
+    print(f'<img src="{image}" width="200"/>', end="")
+    print("</a>")
 
 
 url_list = {
@@ -31,23 +37,8 @@ for source, url in url_list.items():
     )
     qr.add_data(url)
 
-    save_qr(qr, source, colour="_colour", with_image=True)
-    print(f"<h2>{source.title()}</h2>")
-    print(f'<a href="{url}">', end="")
-    print(f'<img src="{save_qr(qr, source)}" width="200"/>', end="")
-    print("</a>")
-    print(f'<a href="{url}">', end="")
-    print(f'<img src="{save_qr(qr, source, colour="_colour")}" width="200"/>', end="")
-    print("</a>")
-    print(f'<a href="{url}">', end="")
-    print(
-        f'<img src="{save_qr(qr, source, colour="_colour_white")}" width="200"/>',
-        end="",
-    )
-    print("</a>")
-    print(f'<a href="{url}">', end="")
-    print(
-        f'<img src="{save_qr(qr, source, colour="_simple", with_image=False)}" width="200"/>',
-        end="",
-    )
-    print("</a>")
+    print(f"<h3>{source.title()}</h3>")
+    print_image(url, save_qr(qr, source, colour="_simple", with_image=False))
+    print_image(url, save_qr(qr, source))
+    print_image(url, save_qr(qr, source, colour="_colour"))
+    print_image(url, save_qr(qr, source, colour="_colour_white"))
